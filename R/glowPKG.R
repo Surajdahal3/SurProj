1b5b4c1bf78d23407b90c4b87f4313709e1fe5b9
Surajdahal3 <89533479+Surajdahal3@users.noreply.github.com>
2021-10-27 14:03:33 -0400

    Create glowPKG.R

---
+++ R/glowPKG.R
@@ -0,0 +1,11 @@
+#' glowPKG Package
+#'
+#' More about what it does (maybe more than one line)
+#'
+#'
+#' @docType package
+#'
+#' @author Suraj D \email{youremail@gmail.com}
+#'
+#' @name glowPKG
+NULL
